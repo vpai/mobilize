3571825c0affda75e30dcf9fbc7c0fa50b714388
Varun Pai <varunpai12@gmail.com>
2019-07-24 16:20:19 -0400

    tabs => spaces

--- app/controllers/shortlinks_controller.rb
+++ app/controllers/shortlinks_controller.rb
@@ -1,43 +1,43 @@
 class ShortlinksController < ApplicationController
-	protect_from_forgery unless: -> { request.format.json? || request.format.xml? }
-	before_action :find_short_link, only: [:redirect, :stats]
+  protect_from_forgery unless: -> { request.format.json? || request.format.xml? }
+  before_action :find_short_link, only: [:redirect, :stats]
 
-	# Executes redirect for short link.
-	def redirect
-		@short_link.update_stats
+  # Executes redirect for short link.
+  def redirect
+    @short_link.update_stats
 
-		redirect_to @short_link.original_url
-	end
+    redirect_to @short_link.original_url
+  end
 
-	# Returns a JSON object containing stats on a given shortlink.
-	def stats
-		render json: { 
-			created_at: @short_link.created_at,
-			total_visits: @short_link.total_hits,
-			hit_counts: @short_link.hit_by_day_hash
-		}
-	end 
+  # Returns a JSON object containing stats on a given shortlink.
+  def stats
+    render json: {
+      created_at: @short_link.created_at,
+      total_visits: @short_link.total_hits,
+      hit_counts: @short_link.hit_by_day_hash
+    }
+  end
 
-	# Creates a new shortlink from a full URL.
-	def new
-		s = ShortLink.create_from_url(params[:link])
+  # Creates a new shortlink from a full URL.
+  def new
+    s = ShortLink.create_from_url(params[:link])
 
-		render json: { hash: s.short_url }
-	end
+    render json: { hash: s.short_url }
+  end
 
-	private
+  private
 
-	# Creates a new shortlink from a full URL.
-	def	find_short_link
-		@short_link = ShortLink.find_by_short_url(params[:hash])
+  # Creates a new shortlink from a full URL.
+  def find_short_link
+    @short_link = ShortLink.find_by_short_url(params[:hash])
 
-		if @short_link.nil?
-			respond_to do |format|
-			  format.html { render file: File.join(Rails.root, 'public', '404.html') }
-			  format.json { render json: { error: 'not_found' }, status: :not_found }
-			end
-		end
+    if @short_link.nil?
+      respond_to do |format|
+        format.html { render file: File.join(Rails.root, 'public', '404.html') }
+        format.json { render json: { error: 'not_found' }, status: :not_found }
+      end
+    end
 
-		@short_link
-	end
+    @short_link
+  end
 end
--- app/models/short_link.rb
+++ app/models/short_link.rb
@@ -1,34 +1,34 @@
 class ShortLink < ApplicationRecord
-	HASH_SALT = "A random salt."
-	SECONDS_IN_DAY = 86400
+  HASH_SALT = "A random salt."
+  SECONDS_IN_DAY = 86400
 
-	def self.create_from_url(original_url)
-		short_url = Digest::MD5.hexdigest(original_url + HASH_SALT)[0..7]
+  def self.create_from_url(original_url)
+    short_url = Digest::MD5.hexdigest(original_url + HASH_SALT)[0..7]
 
-		ShortLink
-			.where(short_url: short_url)
-			.first_or_create(short_url: short_url, original_url: original_url)
-	end 
+    ShortLink
+      .where(short_url: short_url)
+      .first_or_create(short_url: short_url, original_url: original_url)
+  end
 
-	def update_stats
-		start_of_day = (Time.now.to_i / SECONDS_IN_DAY) * SECONDS_IN_DAY
+  def update_stats
+    start_of_day = (Time.now.to_i / SECONDS_IN_DAY) * SECONDS_IN_DAY
 
-		$redis.zincrby(self.short_url, 1, start_of_day)
-	end 
+    $redis.zincrby(self.short_url, 1, start_of_day)
+  end
 
-	def hit_by_day_hash
-		hits_by_day.map do |k|
-			[Time.at(k[0].to_i).utc.to_datetime.strftime('%F'), k[1]]
-		end.to_h
-	end 
+  def hit_by_day_hash
+    hits_by_day.map do |k|
+      [Time.at(k[0].to_i).utc.to_datetime.strftime('%F'), k[1]]
+    end.to_h
+  end
 
-	def total_hits
-		hits_by_day.map { |k| k[1].to_i }.sum
-	end
+  def total_hits
+    hits_by_day.map { |k| k[1].to_i }.sum
+  end
 
-	private
+  private
 
-	def hits_by_day
-		@hits_by_day ||= $redis.zrange(self.short_url, 0, -1, :with_scores => true)
-	end
+  def hits_by_day
+    @hits_by_day ||= $redis.zrange(self.short_url, 0, -1, :with_scores => true)
+  end
 end
